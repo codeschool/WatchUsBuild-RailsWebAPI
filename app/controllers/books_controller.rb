6827d482d18912daafeaaa2ef9354a7e4168f4d2
Carlos Souza <carloshrsouza@gmail.com>
2014-04-17 11:56:40 -0400

    Add query string filter.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,9 @@
 class BooksController < ApplicationController
   def index
-    render json: Book.all, status: 200
+    books = Book.all
+    if rating = params[:rating]
+      books = books.where(rating: rating)
+    end
+    render json: books, status: 200
   end
 end
--- test/integration/listing_books_test.rb
+++ test/integration/listing_books_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class ListingBooksTest < ActionDispatch::IntegrationTest
   setup do
     Book.create!(title: 'Pragmatic Programmer', rating: 5)
+    Book.create!(title: 'Pickaxe', rating: 5)
     Book.create!(title: 'ASP.NET for Dummies', rating: 2)
   end
 
@@ -12,6 +13,17 @@ class ListingBooksTest < ActionDispatch::IntegrationTest
     assert_equal 200, response.status
     assert_equal Mime::JSON, response.content_type
 
-    assert_equal 2, JSON.parse(response.body).size
+    assert_equal Book.count, JSON.parse(response.body).size
+  end
+
+  test 'lists top rated books' do
+    get '/books?rating=5'
+
+    books = JSON.parse(response.body, symbolize_names: true)
+    assert_equal 2, books.size
+
+    titles = books.map { |b| b[:title] }
+    assert_includes titles, 'Pragmatic Programmer'
+    refute_includes titles, 'ASP.NET for Dummies'
   end
 end
