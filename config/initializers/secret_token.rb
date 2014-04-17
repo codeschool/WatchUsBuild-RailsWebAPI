7c6b71de0a4501e5a4ad739538c3aa629ef80ea0
Carlos Souza <carloshrsouza@gmail.com>
2014-04-17 10:44:19 -0400

    New Rails 4.0.4 app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ReadingListAPI::Application.config.secret_key_base = '3f99fbac7250b0d4171c3f742b8a6bb393c02e403521e8afffe1db797bd2422bc8f09c7b1c1438dd6e174c074d2c7e98b999acf3a0377e59420df0a43a3ecb10'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ReadingListAPI::Application.config.session_store :cookie_store, key: '_ReadingListAPI_session'
