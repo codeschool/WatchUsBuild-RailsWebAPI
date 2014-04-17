cf12856bfb85bd8addeacd80205feb570732540d
Carlos Souza <carloshrsouza@gmail.com>
2014-04-17 11:30:47 -0400

    Add route restrictions.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 ReadingListAPI::Application.routes.draw do
+
   resources :books
+
+  with_options except: [:create, :update, :destroy] do |read_only|
+    read_only.resources :authors
+    read_only.resources :genres
+  end
 end
